13b50dc2c0772dd48047528a03acb3085b6f2794
pawaffle <pdegterev@gmail.com>
2024-10-02 12:03:02 +0300

    p1.1

--- Act1/p1-my-theme-app/src/App.jsx
+++ Act1/p1-my-theme-app/src/App.jsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.jsx
+import ThemeProvider from './Context/ThemeProvider';
+import Form from './Components/Form';
+import Button from './Components/Button';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider> {/* Provides theme context to all child components */}
+      <Form />
+      <Button>Toggle Theme</Button> {/* This button can toggle the theme */}
+    </ThemeProvider>
+  );
 }
-
-export default App
